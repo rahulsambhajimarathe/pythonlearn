d94959c03f4d072544bc2ed91c93754146ff4304
Rahul <rahulsambhajimarathe@gmail.com>
2023-06-25 19:39:11 +0530

    str int datatype

---
+++ Tuttion/string/main.py
@@ -0,0 +1,43 @@
+"""#1
+string = "Rahul Marathe"
+print(len(string))
+
+#2
+string = "RaR"
+reversed_string = ""
+for str in string:
+    reversed_string = str + reversed_string
+
+
+if string == reversed_string:
+    print("Your string is palindrome:", string, "==", reversed_string)
+else:
+    print("Your string is not palindrome", string, "==", reversed_string)"""
+    
+   
+   
+   #4 
+"""    
+con = "Concatenate"
+b= ""
+for str in con:    
+    if str != "o":
+       print(str)
+       """
+
+"""number = "654"
+
+for str in range(len(number)):
+    print(int(str))
+    """
+    
+    #3
+    
+"""string_number = "6889"
+digit_sum = 0
+
+for digit in string_number:
+    if digit == "0" or digit == "1" or digit == "2" or digit == "3" or digit == "4" or digit == "5" or digit == "6" or digit == "7" or digit == "8" or digit == "9":
+        digit_sum += int(digit)
+
+print(digit_sum)"""
